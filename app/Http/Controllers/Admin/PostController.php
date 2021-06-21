6c80f9455cae959d50650ae1a794717eb474a692
Alina M <alinamitskaya@gmail.com>
2021-06-21 18:05:36 +1000

    добавит вывод постов всех пользователей

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function getPosts()
+    {
+        $posts = Post::all();
+        return view('admin.post.posts', compact('posts'));
+    }
+}
---
+++ resources/views/admin/post/posts.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.admin')
+@section('content')
+    <link href="../../../sass/usersList.css" rel="stylesheet" />
+    <div class="container">
+        <div class="row" style="margin-top: 20px">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-uppercase mb-0">Manage Posts</h5>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table no-wrap user-table mb-0">
+                            <thead>
+                            <tr>
+                                <th scope="col" class="border-0 text-uppercase font-medium pl-4">Id</th>
+                                <th scope="col" class="border-0 text-uppercase font-medium">Title</th>
+                                <th scope="col" class="border-0 text-uppercase font-medium">User id</th>
+                                <th scope="col" class="border-0 text-uppercase font-medium">Created</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td class="pl-4">{!! $post->id !!}</td>
+                                    <td>
+                                        <h5 class="font-medium mb-0">{!! $post->title !!}</h5>
+                                    </td>
+                                    <td>
+                                        <span class="text-muted">{!! $post->user_id !!}</span><br>
+                                    </td>
+                                    <td>
+                                        <span class="text-muted">{!! $post->created_at !!}</span><br>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
