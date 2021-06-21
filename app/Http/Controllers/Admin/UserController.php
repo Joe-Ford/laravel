b4784e5297e037c98e727fc4ae64538074c5fe4a
Alina M <alinamitskaya@gmail.com>
2021-06-21 18:06:23 +1000

    добавит просмотр данных пользователей

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+
+class UserController extends Controller
+{
+    public function getUsers()
+    {
+        $users = User::all();
+        return view('admin.user.users', compact('users'));
+    }
+}
---
+++ resources/views/admin/user/users.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.admin')
+@section('content')
+    <link href="../../../sass/usersList.css" rel="stylesheet" />
+    <div class="container">
+        <div class="row" style="margin-top: 20px">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-uppercase mb-0">Manage Users</h5>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table no-wrap user-table mb-0">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="border-0 text-uppercase font-medium pl-4">Id</th>
+                                    <th scope="col" class="border-0 text-uppercase font-medium">Name</th>
+                                    <th scope="col" class="border-0 text-uppercase font-medium">Email</th>
+                                    <th scope="col" class="border-0 text-uppercase font-medium">Created</th>
+                                    <th scope="col" class="border-0 text-uppercase font-medium">Role</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <td class="pl-4">{!! $user->id !!}</td>
+                                        <td>
+                                            <h5 class="font-medium mb-0">{!! $user->name !!}</h5>
+                                        </td>
+                                        <td>
+                                            <span class="text-muted">{!! $user->email !!}</span><br>
+                                        </td>
+                                        <td>
+                                            <span class="text-muted">{!! $user->created_at !!}</span><br>
+                                        </td>
+                                        <td>
+                                            <span class="text-muted">
+                                                @if($user->isAdmin == 1)
+                                                    Admin
+                                                @else
+                                                    User
+                                                @endif
+                                            </span><br>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
