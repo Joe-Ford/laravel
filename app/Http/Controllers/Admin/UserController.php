22148ff33c98a799a32744c596a7dad6bcd6a6b6
Alina M <alinamitskaya@gmail.com>
2021-06-22 15:18:56 +1000

    добавит функции работы с таблицей пользователей

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -11,8 +11,37 @@ class UserController extends Controller
 {
     public function getUsers()
     {
-        $users = User::all();
+        $users = $this->getAllUsers();
 
         return view('admin.user.users', compact('users'));
+    }
+
+    public function viewUser(int $id)
+    {
+        $user = User::find($id);
+
+        return view('admin.user.edit_user', compact('user'));
+    }
+
+    public function editUser(Request $request, int $id)
+    {
+        $user = User::find($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        if($request->role === "Admin") {
+            $user->isAdmin = 1;
+        } else {
+            $user->isAdmin = 0;
+        }
+        $user->save();
+
+        $users = $this->getAllUsers();
+
+        return view('admin.user.users', compact('users'));
+    }
+
+    private function getAllUsers()
+    {
+        return User::all();
     }
 }
