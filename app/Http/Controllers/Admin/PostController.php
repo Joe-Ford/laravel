58fb33a653cba4d8f442ebfd9fb4ed57494caaa6
Alina M <alinamitskaya@gmail.com>
2021-06-22 17:29:47 +1000

    бавит методы работы с постами пользователей

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -11,8 +11,37 @@ class PostController extends Controller
 {
     public function getPosts()
     {
-        $posts = Post::all();
+        $posts = $this->getAllPosts();
 
         return view('admin.post.posts', compact('posts'));
+    }
+
+    public function viewPost(int $id)
+    {
+        $post = Post::find($id);
+
+        return view('admin.post.edit_post', compact('post'));
+    }
+
+    public function editPost(Request $request, int $id)
+    {
+        $post = Post::find($id);
+        $post->title = $request->title;
+        $post->post = $request->post;
+        $post->save();
+
+        $posts = $this->getAllPosts();
+
+        return view('admin.post.posts', compact('posts'));
+    }
+
+    public function deletePost()
+    {
+
+    }
+
+    private function getAllPosts()
+    {
+        return Post::all();
     }
 }
